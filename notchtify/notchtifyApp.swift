3321307b605c800ea011a3a99aa96f3d2413eb11
patricknelwan <patrick.nelwan50@gmail.com>
2025-08-26 18:40:12 +0700

    Initial Commit

---
+++ notchtify/notchtifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchtifyApp.swift
+//  notchtify
+//
+//  Created by Patrick Nelwan on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct notchtifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ notchtifyTests/notchtifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  notchtifyTests.swift
+//  notchtifyTests
+//
+//  Created by Patrick Nelwan on 26/08/25.
+//
+
+import Testing
+@testable import notchtify
+
+struct notchtifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
